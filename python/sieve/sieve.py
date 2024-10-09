0f2042e665c2bb761bdbe40eeaa1f4a471ae4b84
unknown <emailjigyasa@gmail.com>
2024-10-08 20:46:19 -0500

    Updating sieve.py and test files in the python package

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,5 +1,67 @@
+import math
+
+import sympy
+
 class Sieve:
+
     def __init__(self) -> None:
         pass
+
     def nth_prime(self, n: int) -> int:
-        pass
+        return sympy.prime (n+1) # sympy is 1 based index hence n+1
+
+    """
+    These are 2 other functions which are not efficient.
+    I was not able to get the prime value even for 10000000th position from either methods
+    """
+
+    def nth_prime_basic(self, n):
+        # Chk if num is prime
+        def is_prime(num):
+            if num < 2:
+                return False
+            for i in range(2, int(num ** 0.5) + 1):
+                if num % i == 0:
+                    return False
+            return True
+
+        # Prime number counter
+        count = 0
+        prime_num = 1
+
+        # Loop to find the nth prime
+        while count <= n:
+            prime_num += 1
+            if is_prime(prime_num):
+                count += 1
+        return prime_num
+
+
+    def nth_prime_soe(self, n):
+        # Return 2 for the 0th position per the assignment
+        if n == 0:
+            return 2
+
+        # Estimate an upper limit to for the rage using Prime number theorem
+        # (gives a rough estimate of the upper bound for the nth prime)
+        upper_limit = int(n * math.log(n) + n * math.log(math.log(n))) + 10
+
+        # Boolean array w 'upper_limit' as size of the array
+        sieve = [True] * (upper_limit + 1)
+        sieve[0], sieve[1] = False, False
+
+        prime_list = []
+
+        # Mark multiples of each prime starting from 2 (based on Sieve of Eratosthenes)
+        for p in range(2, upper_limit + 1):
+            if sieve[p]:
+                prime_list.append(p)
+                for multiple in range(p * p, upper_limit + 1, p):
+                    sieve[multiple] = False
+
+            # Since we have gustimated the Range of numbers based on 'n',
+            # we have to break out of the loop if we get more than 'n' primes
+            if len(prime_list) > n:
+                break
+
+        return prime_list[n]
--- python/test_sieve.py
+++ python/test_sieve.py
@@ -1,19 +1,40 @@
+import random
 import unittest
+
+import sympy
+
 from sieve import Sieve
 
 class SieveTest(unittest.TestCase):
 
     def test_sieve_nth_prime(self) -> None:
         sieve = Sieve()
-        self.assertEqual(2, sieve.nth_prime(0))
-        self.assertEqual(71, sieve.nth_prime(19))
-        self.assertEqual(541, sieve.nth_prime(99))
-        self.assertEqual(3581, sieve.nth_prime(500))
-        self.assertEqual(7793, sieve.nth_prime(986))
-        self.assertEqual(17393, sieve.nth_prime(2000))
-        self.assertEqual(15485867, sieve.nth_prime(1000000))
-        self.assertEqual(179424691, sieve.nth_prime(10000000))
-        # self.assertEqual(2038074751, sieve.nth_prime(100000000)) not required, just a fun challenge
+        self.assertEqual(2, sieve.nth_prime(0)) # Total Time taken: 0.000000 seconds ¯\_(ツ)_/¯
+        self.assertEqual(71, sieve.nth_prime(19)) # Total Time taken: 0.264271 seconds
+        self.assertEqual(541, sieve.nth_prime(99)) # Total Time taken: 0.017035 seconds
+        self.assertEqual(3581, sieve.nth_prime(500)) # Total Time taken: 0.034978 seconds
+        self.assertEqual(7793, sieve.nth_prime(986)) # Total Time taken: 0.021989 seconds
+        self.assertEqual(17393, sieve.nth_prime(2000)) # Total Time taken: 0.026004 seconds
+        self.assertEqual(15485867, sieve.nth_prime(1000000)) # Total Time taken: 0.077955 seconds
+        self.assertEqual(179424691, sieve.nth_prime(10000000)) # Total Time taken: 0.248547 seconds
+        self.assertEqual(2038074751, sieve.nth_prime(100000000)) # Total Time taken: 1.450387 seconds
+        self.assertEqual(22801763513, sieve.nth_prime(1000000000)) # Total Time taken: 8.017118 seconds
+        self.assertEqual(252097800629, sieve.nth_prime(10000000000)) # Total Time taken: 46.271795 seconds
 
     def test_sieve_fuzz_nth_prime(self) -> None:
-        pass
+        sieve = Sieve()
+
+        max_range = 1000
+        max_no_of_fuzz_tests = 50
+
+        for _ in range (max_no_of_fuzz_tests):
+            # Generate a random number to generate prime for
+            n = random.randint (0, max_range)
+
+            # Generate prime # for the nth place
+            nth_prime_num = sieve.nth_prime(n)
+
+            # Validate the num generate is actually prime
+            self.assertTrue(sympy.isprime(nth_prime_num), f"Generated number {nth_prime_num} is not prime")
+
+
